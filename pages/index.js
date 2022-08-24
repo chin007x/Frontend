b896738cac62948ab4121d73c452c656da8b28be
chin007x <chinnawatq@gmail.com>
2022-08-24 09:37:19 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </h1> 
 
         <p className={styles.description}>
           Get started by editing{' '}
